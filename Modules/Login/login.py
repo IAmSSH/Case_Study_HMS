942a16b106896ffcae40af32ded9842c6f5a0cf2
vagrawal645 <=>
2020-06-27 14:49:12 +0530

    err

--- Modules/Login/login.py
+++ Modules/Login/login.py
@@ -1,7 +1,19 @@
-from flask import Flask,render_template,Blueprint
-
+from flask import Flask,redirect, url_for, render_template , request , session , flash , Blueprint
+from Modules.Models.userstore import userstore
 login = Blueprint("login",__name__,static_folder="static",template_folder="templates")
 
-@login.route("/login")
+@login.route("/login" , methods=["GET","POST"])
 def auth():
+    if request.method == "POST":
+        login = request.form["user_id"]
+        password = request.form["password"]
+        session["login"] = login
+        session["password"] = password
+        found_user = userstore.query.filter_by(login = login , password = password).first()
+        if found_user :
+            return "Success!"
+        else:
+            flash("Invalid Credentials")
+            return render_template("login.html")
+
     return render_template("login.html")
--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, redirect, url_for, render_template , request , session 
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 from Modules.Login.login import login
+from Modules.Models.userstore import userstore , db
+# db.create_all()
 app = Flask(__name__)
+app.secret_key = "hello"
+db.init_app(app)
 app.register_blueprint(login,url_prefix="/")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,4 +15,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def home():
     return render_template("Base.html")
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.db.create_all()
+    app.run(debug=True)
