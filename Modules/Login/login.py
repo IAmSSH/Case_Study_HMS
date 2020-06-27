3562fe5e9a25b61b08b7eea7ff95d7d96a9c2c62
vagrawal645 <=>
2020-06-27 16:21:18 +0530

    Register Added

--- Modules/Login/login.py
+++ Modules/Login/login.py
@@ -1,23 +1,41 @@
-from flask import Flask,redirect, url_for, render_template , request , session , flash , Blueprint
-from Modules.Models.userstore import userstore
-login = Blueprint("login",__name__,static_folder="static",template_folder="templates")
-from flask_sqlalchemy import SQLAlchemy
+from Modules.Models.userstore import userstore, db
+from flask import Flask, redirect, url_for, render_template, request, session, Blueprint
+from flask import flash
+from datetime import datetime
+login = Blueprint("login", __name__, static_folder="static",
+                  template_folder="templates")
 
-db = SQLAlchemy()
 
-
-@login.route("/login" , methods=["GET","POST"])
+@login.route("/login", methods=["GET", "POST"])
 def auth():
     if request.method == "POST":
         login = request.form["user_id"]
         password = request.form["password"]
+        dateTimeObj = datetime.now()
         session["login"] = login
         session["password"] = password
-        found_user = userstore.query.filter_by(login = login , password = password).first()
-        if found_user :
+        found_user = userstore.query.filter_by(
+            login=login, password=password).first()
+        if found_user:
             return "Success!"
         else:
             flash("Invalid Credentials")
             return render_template("login.html")
-
     return render_template("login.html")
+
+@login.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        login = request.form["user_id"]
+        password = request.form["password"]
+        dateTimeObj = datetime.now()
+        session["login"] = login
+        session["password"] = password
+        usr = userstore(login, password, dateTimeObj)
+        db.session.add(usr)
+        db.session.commit()
+        flash(f"Registering new User at {dateTimeObj} by user Id as {login}")
+        return "Success!"
+    else :
+        return render_template("register.html")
+
--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, redirect, url_for, render_template , request , session 
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 from Modules.Login.login import login
-from Modules.Models.userstore import userstore , db
 # db.create_all()
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -10,6 +9,18 @@ app.register_blueprint(login,url_prefix="/")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class userstore(db.Model):
+    time_stamp = db.Column("time_stamp", db.DateTime, primary_key=True)
+    login = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, login, password, time_stamp):
+        self.login = login
+        self.password = password
+        self.time_stamp = time_stamp
+
+
 @app.route("/")
 def home():
     return render_template("Base.html")
