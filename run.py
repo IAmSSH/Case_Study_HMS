1dc64112e040ba0bc33785f47896633b9c440242
iamssh <saadhasan620@gmail.com>
2020-06-28 16:04:02 +0530

    style registration and view all page, address not working

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 class Patient(db.Model):
     ssn_id = db.Column(db.Integer)
@@ -21,29 +22,34 @@ class Patient(db.Model):
     state = db.Column(db.String(50))
     status = db.Column(db.String(15))
 
+
 class Test(db.Model):
     ssn_id = db.Column(db.Integer)
     patient_id = db.Column(db.Integer, primary_key=True)
 
 
-db.create_all()        
+db.create_all()
+
 
 @app.route('/')
 def home():
     print(Patient.query.all())
-    return render_template('Base.html')
+    return render_template('base.html')
+
 
 @app.route('/register_patient')
 def register_patient():
     return render_template('patientRegistration.html')
 
+
 @app.route('/patients', methods=['GET', 'POST'])
 def create_patient():
     if request.method == 'POST':
+        # Check for duplicate in DB
         ssn_id = request.form['ssnid']
         name = request.form['name']
         age = request.form['age']
-        admission_date = request.form['date'] # check 
+        admission_date = request.form['date']  # check
         bed_type = request.form['bed_type']
         address = request.form['address']
         city = request.form['city']
@@ -51,34 +57,75 @@ def create_patient():
         status = 'Active'
 
         patient = Patient(
-        ssn_id = ssn_id,
-        name = name,
-        age = age,
-        admission_date = admission_date,
-        bed_type = bed_type,
-        address = address,
-        city = city,
-        state = state,
-        status = status 
-            )
+            ssn_id=ssn_id,
+            name=name,
+            age=age,
+            admission_date=admission_date,
+            bed_type=bed_type,
+            address=address,
+            city=city,
+            state=state,
+            status=status
+        )
 
         db.session.add(patient)
         db.session.commit()
-        
+
         return render_template('all_patients.html', user_list=Patient.query.all())
     else:
-        print('bed_type for patient 1 : ' + Patient.query.get(1).bed_type)
         return render_template('all_patients.html', user_list=Patient.query.all())
+
 
 @app.route('/test')
 def test():
     return render_template('test.html')
 
+
+@app.route("/update", methods=['GET', 'POST'])
+def update():
+    if request.method == "POST":
+        data = Patient.query.all()
+        id = 0
+        if 'Get' in request.form:
+            req_id = int(request.form['patientid'])
+            # print(data)
+            for i in data:
+                print('req_id : ' + str(req_id) +
+                      ' i.patient_id : ' + str(i.patient_id))
+                if i.ssn_id == req_id:
+                    id = req_id
+                    print(i)
+                    return render_template('updatePatient.html', patient_data=i)
+
+        elif 'Update' in request.form:
+            ssn_id = request.form['patientid']
+            name = request.form['name']
+            age = request.form['age']
+            admission_date = request.form['date']  # check
+            bed_type = request.form['bed_type']
+            address = request.form['address']
+            city = request.form['city']
+            state = request.form['state']
+            update_Record = Patient.query.filter_by(ssn_id=ssn_id).first()
+            print(update_Record, ' so yeah!', Patient.query.all(), ' id', id)
+            update_Record.name = name
+            update_Record.age = age
+            update_Record.admission_date = admission_date
+            update_Record.bed_type = bed_type
+            update_Record.address = address
+            update_Record.city = city
+            update_Record.state = state
+            db.session.commit()
+            return render_template('updatePatient.html', patient_data=update_Record)
+
+    return render_template('updatePatient.html', patient_data=Patient())
+
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         ssn_id = request.form['ssnid']
-        patient = Test(ssn_id = ssn_id)
+        patient = Test(ssn_id=ssn_id)
 
         db.session.add(patient)
         db.session.commit()
@@ -86,6 +133,13 @@ def upload():
         return render_template('all_patients.html', user_list=Test.query.all())
     else:
         return render_template('all_patients.html', user_list=Test.query.all())
+
+@app.route('/clear_patients')
+def clear():
+    Patient.query.delete()
+    db.session.commit()    
+    flash('Patients cleared')
+    return redirect(url_for('create_patient'))
 
 if __name__ == '__main__':
     app.run(debug=True)
